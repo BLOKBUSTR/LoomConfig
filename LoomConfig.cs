885099a2bf8004dd0ce2bc5999a2a751a6582719
BLOKBUSTR <blokbustrmusic@gmail.com>
2026-02-10 21:03:27 -0800

    Initial commit

---
+++ LoomConfig.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+#pragma warning disable CS8618
+namespace LoomConfig
+{
+    [BepInPlugin("BLOKBUSTR.LoomConfig", "LoomConfig", "0.0.1")]
+    public class LoomConfig : BaseUnityPlugin
+    {
+        internal static LoomConfig Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger => Instance._logger;
+        [SuppressMessage("ReSharper", "InconsistentNaming")] private ManualLogSource _logger => base.Logger;
+        internal Harmony? Harmony { get; set; }
+        
+        // General
+        public static ConfigEntry<int> configClapDamage;
+        
+        // Visual
+        public static ConfigEntry<float> configPlayerLookDistance;
+        
+        // Debug
+        private static ConfigEntry<bool> configEnableDebug;
+        
+        private void Awake()
+        {
+            Instance = this;
+            
+            gameObject.transform.parent = null;
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+            
+            InitConfig();
+            Patch();
+            
+            Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+            Debug("Debug logging is enabled.");
+        }
+        
+        private void InitConfig()
+        {
+            // General
+            configClapDamage = Config.Bind("General", "ClapDamage", 100,
+                new ConfigDescription("The amount of damage dealt by the clap attack.",
+                    new AcceptableValueRange<int>(0, 500)));
+            
+            // Visual
+            configPlayerLookDistance = Config.Bind("Visual", "PlayerLookDistance", 7f,
+                new ConfigDescription("The distance at which the Loom considers itself close enough to look at the player.",
+                    new AcceptableValueRange<float>(7f, 15f)));
+            
+            // Debug
+            configEnableDebug = Config.Bind("Debug", "EnableDebug", false,
+                new ConfigDescription("Whether to enable debug logging."));
+        }
+        
+        internal void Patch()
+        {
+            Harmony ??= new Harmony(Info.Metadata.GUID);
+            Harmony.PatchAll();
+        }
+        
+        internal void Unpatch()
+        {
+            Harmony?.UnpatchSelf();
+        }
+        
+        internal static void Debug(string message, MonoBehaviour? instance = null)
+        {
+            if (!configEnableDebug.Value) return;
+            var prefix = instance != null ? $"{instance.GetInstanceID()}: " : "";
+            Logger.LogDebug(prefix + message);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# LoomConfig
+
+This is a simple mod that allows you to configure various parameters of the Loom monster, from mechanics to animations.
+
+❗ **Client requirement TBD**
+
+Suggestions are welcome! Tell me what's on your mind in the Discord thread.
+
+## 🔧 Configuration
+
+Configs can be updated in-game with RepoConfig, but they will only apply on the next instance of a Loom.
+
+<details>
+    <summary>Click to expand config list:</summary>
+
+- Visual:
+  - **PlayerLookDistance**: The distance at which the Loom considers itself close enough to look at the player.
+- Debug:
+  - **EnableDebug**: Whether to enable debug logging.
+
+</details>
+
+## ⚠️ Compatibility
+
+There are no known incompatibilities yet, but this mod may potentially conflict with others that extensively patch these methods:
+- `EnemyShadow.Update()`
+
+---
+
+Thank you for using this mod! \
+Please report any issues on GitHub or the Discord thread.
